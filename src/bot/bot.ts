5c1ffd5fd5237aa55f0b5774fe0303f8ef56c63e
adithyagenie <adithyagenie@gmail.com>
2024-06-16 00:33:06 +0530

    Add throttler for bot
    Signed-off-by: adithyagenie <adithyagenie@gmail.com>

--- src/bot/bot.ts
+++ src/bot/bot.ts
@@ -1,12 +1,15 @@
 import { Bot, Context } from "grammy";
 import { parseMode, ParseModeFlavor } from "@grammyjs/parse-mode";
 import { helpCommand, messageSink, setCommands, startCommand } from "./commands/miscCommands";
+import { apiThrottler } from "@grammyjs/transformer-throttler";
 
 
 export const bot = new Bot<ParseModeFlavor<Context>>(`${process.env.BOT_TOKEN}`);
 export type myBot = Bot<ParseModeFlavor<Context>>;
 
 export async function botInit() {
+	const throttler = apiThrottler();
+	bot.api.config.use(throttler);
 	bot.api.config.use(parseMode("HTML"));
 	setCommands(bot);
 	bot.hears(/^\/start/, async(ctx) => await startCommand(ctx));
